822b6e83b828b905de34da65a117f42c01752473
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2026-03-11 11:28:47 +0700

    add id strategy use

---
+++ j2j-framework/src/main/java/j2j/model/Fullname.java
@@ -0,0 +1,10 @@
+package j2j.model;
+
+public class Fullname {
+    private String name;
+    private String surname;
+    public Fullname(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
